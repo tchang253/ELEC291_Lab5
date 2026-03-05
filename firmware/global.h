8dbda93a76da00aebc23e1c65fc2b9cf1e4e9a8a
codyliu06 <codyliu06@gmail.com>
2026-03-05 00:36:30 -0800

    i fixed the entire code

--- firmware/global.h
+++ firmware/global.h
@@ -4,6 +4,7 @@
 #define SYSCLK    72000000L // SYSCLK frequency in Hz
 #define BAUDRATE    115200L // Baud rate of UART in bps
 #define SARCLK 18000000L
+#define VDD 3.311       //voltage reference for ADC
 
 
 
